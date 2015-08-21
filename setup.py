f5ef5d0e207beecf21d9c48757200dcc2355cbfc
HenrikOssipoff <henrik.ossipoff@gmail.com>
2015-08-21 11:45:36 +0200

    Flake8 and add JSON_SETTINGS_FILE
    This patch fixes up flake8 and isort linting, and adds a new SETTINGS
    directice which allows one to change the filename json_settings is
    looking to load from sys.prefix.
    This is helpful for projects where a single deployment for example wants
    to use different databases depending on a given DJANGO_SETTINGS_MODULE.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -11,7 +11,9 @@ setup(
     packages=["json_settings"],
     include_package_data=True,
     license="Apache Software License",
-    description="A Django application to let you provide local settings in json format",
+    description=(
+        "A Django application to let you provide local settings in json format"
+    ),
     long_description=README,
     url="http://github.com/isotoma/django-json-settings",
     author="Doug Winter",
